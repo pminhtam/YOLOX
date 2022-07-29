16c6e0462827e9c0bb20bb45942074f51ff36244
MInh Tam <pminhtamnb@gmail.com>
2022-07-29 22:31:04 +0700

    add more COCO setting

--- yolox_voc_bnn_clipgrad.sh
+++ yolox_voc_bnn_clipgrad.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=2yoloVOC_bnn
-#SBATCH --output=slurm_bnn2_yolox_voc_%A.out
-#SBATCH --error=slurm_bnn2_yolox_voc_%A.err
+#SBATCH --job-name=yoloxClipVOC_bnn
+#SBATCH --output=slurm_bnn_yolox_clip_voc_%A.out
+#SBATCH --error=slurm_bnn_yolox_clip_voc_%A.err
 #SBATCH --gpus=1
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
@@ -18,9 +18,9 @@ export HTTP_PROXY=http://proxytc.vingroup.net:9090/
 export HTTPS_PROXY=http://proxytc.vingroup.net:9090/
 export http_proxy=http://proxytc.vingroup.net:9090/
 export https_proxy=http://proxytc.vingroup.net:9090/
-rm -rf /home/tampm2/.conda/envs/yolox_voc_bnn2
-conda create --name yolox_voc_bnn2 python=3.7 --force
-conda activate yolox_voc_bnn2
+rm -rf /home/tampm2/.conda/envs/yolox_clip_voc_bnn
+conda create --name yolox_clip_voc_bnn python=3.7 --force
+conda activate yolox_clip_voc_bnn
 pip install -r requirements.txt
 pip install setuptools==59.5.0
 pip install tensorboard
@@ -31,4 +31,5 @@ wandb login c5a6f4c212b00734d9517784e3e892155a301de0
 #tar -xvf /vinai-public-dataset/VOC2012/VOCtrainval_11-May-2012.tar -C data/
 #tar -xvf /vinai-public-dataset/VOC2007/VOCtrainval_06-Nov-2007.tar -C data/
 #tar -xvf /vinai-public-dataset/VOC2007/VOCtest_06-Nov-2007.tar -C data/
-python -m yolox.tools.train -n yolox-s -d 1 -b 32 -o  --cache -expn yolox_s_voc_bnn2_fix --binary_backbone --resume
+python -m yolox.tools.train -n yolox-s -d 1 -b 32 -o -expn yolox_s_clip_voc_bnn_fix --binary_backbone --binary_head --clip_grad --resume
+### _fix : just binaize conv with ksize = 3x3 or larger.
